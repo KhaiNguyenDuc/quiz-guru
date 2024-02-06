d86a8c0d441061a3628de8db05619a144adb6225
KhaiNguyenDuc <duckhailinux@gmail.com>
2024-02-06 13:39:11 +0700

    Update dev environment

--- back-end/src/main/java/com/khai/quizguru/QuizGuruApplication.java
+++ back-end/src/main/java/com/khai/quizguru/QuizGuruApplication.java
@@ -4,13 +4,14 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.web.bind.annotation.CrossOrigin;
+
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
 @SpringBootApplication
+
 public class QuizGuruApplication {
 
 	public static void main(String[] args) {
@@ -47,4 +48,6 @@ public class QuizGuruApplication {
 				}
 			};
 		}
+
+
 }
