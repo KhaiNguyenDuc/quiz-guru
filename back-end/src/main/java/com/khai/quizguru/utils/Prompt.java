63fbdc9567bc8dd7ddd04ca85440e904825c20c7
KhaiNguyenDuc <duckhailinux@gmail.com>
2024-02-12 01:50:00 +0700

    Update prompt

--- back-end/src/main/java/com/khai/quizguru/utils/Prompt.java
+++ back-end/src/main/java/com/khai/quizguru/utils/Prompt.java
@@ -9,10 +9,10 @@ public class Prompt {
     public static final String MIX_CHOICE_QUIZ_PROMPT = "Give me exactly %d questions of type %s about the given text which has the level %s. Return your answer entirely in the form of a JSON object. The JSON object must have a key named \"questions\" which is an array of the questions. Each quiz question should include exactly 4 choices, the answers, and a brief explanation of why the answers is correct. Don't include anything other than the JSON. The JSON properties of each question should be \"query\" (which is the question), \"choices\", \"answers\", \"type\"( \"MULTIPLE_CHOICE\" for question has more than 1 correct choice or \"SINGLE_CHOICE\" for question has only 1 correct choice) and \"explanation\". The choices shouldn't have any ordinal value like A, B, C, D or a number like 1, 2, 3, 4. The answer should be the 0-indexed number of the correct choice. The json Object will returned in %s language";
     public static final String VOCABULARY_QUIZ_PROMPT = "Provide exactly %d questions of type %s based on the given vocabulary," +
             "the purpose is to help user learn about those vocabularies with a difficulty level of %s," +
-            "Create the questions with context mainly about: " +
-            "1. The definition of that word" +
-            "2. The context that word is used ( Use cloze question - embedded answer )" +
-            "3. The family words relate to that word ( Use cloze question - embedded answer)" +
+            "Create the questions switching between: " +
+            "1. Cloze question - embedded answer." +
+            "2. The question about detail definition of that word." +
+            "3. The question using that word in real life" +
             "Return your response in the form of a JSON object. The JSON object should contain a key named \"questions,\" " +
             "which is an array of the questions. Each multiple-choice question should have exactly 4 choices, answers, " +
             "and a brief explanation of why the answers are correct. There must have more than 1 choice corrected, " +
@@ -29,10 +29,10 @@ public class Prompt {
 
     public static final String TEXT_TO_VOCAB_PROMPT = "Provide exactly %d questions of type %s based on exactly %d vocabularies you found in the given text which has the level of %s, the vocabularies should be a single word " +
             "the purpose is to help user learn about those vocabularies contain in the text, the questions should not relate to the given text but relate to the vocabularies itself" +
-            "Create the questions with context mainly about: " +
-            "1. The definition of that word" +
-            "2. The context that word is used ( Use cloze question - embedded answer )" +
-            "3. The family words relate to that word ( Use cloze question - embedded answer)" +
+            "Create the questions switching between: " +
+            "1. Cloze question - embedded answer." +
+            "2. The question about detail definition of that word." +
+            "3. The question using that word in real life" +
             "Return your response in the form of a JSON object. The JSON object should contain a key named \"words\" which is an array of vocabularies you found in the given text" +
             "a key named \"questions,\" which is an array of the questions. Each multiple-choice question should have exactly 4 choices, answers, " +
             "and a brief explanation of why the answers are correct. There must have more than 1 choice corrected, " +
