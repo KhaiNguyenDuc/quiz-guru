3cfa0ad8ae12734dde799b54baf5abf6526d478e
KhaiNguyenDuc <duckhailinux@gmail.com>
2024-02-11 18:17:58 +0700

    Update deploy workflows

--- back-end/src/main/java/com/khai/quizguru/payload/request/RecordRequest.java
+++ back-end/src/main/java/com/khai/quizguru/payload/request/RecordRequest.java
@@ -1,6 +1,7 @@
 package com.khai.quizguru.payload.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.util.List;
@@ -11,7 +12,7 @@ public class RecordRequest {
     @NotBlank(message = "Quiz's ID can't be empty")
     private String quizId;
 
-    @NotBlank(message = "TimeLeft can't be empty")
+    @NotNull(message = "TimeLeft can't be empty")
     private Integer timeLeft;
     private List<RecordItemRequest> recordItems;
 }
